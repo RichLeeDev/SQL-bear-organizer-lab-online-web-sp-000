67bf595af4043553ed0d809a7973eb6c5ca25063
Richard Lee <richlee81192@gmail.com>
2019-10-10 21:23:38 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+"INSERT INTO bears(*) VALUES ('Apple', 10, 'male', 'black', 'calm', 'true');"
+
+"INSERT INTO bears(*) VALUES ('Banana', 10, 'male', 'black', 'calm', 'true');"
+
+"INSERT INTO bears(*) VALUES ('Crisp', 10, 'male', 'black', 'calm', 'true');"
+
+"INSERT INTO bears(*) VALUES ('Diablo', 10, 'male', 'black', 'calm', 'true');"
+
+"INSERT INTO bears(*) VALUES ('Echo', 10, 'male', 'black', 'calm', 'true');"
+"INSERT INTO bears(*) VALUES ('Frank', 10, 'male', 'black', 'calm', 'true');"
+"INSERT INTO bears(*) VALUES ('Gaul', 10, 'male', 'black', 'calm', 'true');"
+"INSERT INTO bears(*) VALUES (NULL, 10, 'male', 'black', 'calm', 'true');"
